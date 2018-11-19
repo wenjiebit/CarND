a18fbb6f5eb51be7671aeeb3a94726b1ea7af154
Rancho <wenjiebit@163.com>
2018-11-19 16:33:05 +0800

    Update install-ubuntu.sh

--- Term3/CarND-Path_Planning-P1/install-ubuntu.sh
+++ Term3/CarND-Path_Planning-P1/install-ubuntu.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
-sudo apt-get install libuv1-dev
+
+#安装libuv的命令为Install libuv on ubuntu 14.04 .
+sudo -E add-apt-repository ppa:acooks/libwebsockets6
+sudo apt-get update
+sudo apt-get install libuv1.dev
 git clone https://github.com/uWebSockets/uWebSockets 
 cd uWebSockets
-# git checkout e94b6e1
+git checkout e94b6e1
 mkdir build
 cd build
 cmake ..
